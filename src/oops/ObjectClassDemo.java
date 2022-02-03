248e99e3160407f16053cfe27646dc468410572e
sandeep bhingle <sandeepbhingle@gmail.com>
2022-02-03 09:31:29 +0530

    Array programs

---
+++ src/oops/ObjectClassDemo.java
@@ -0,0 +1,37 @@
+package oops;
+
+public class ObjectClassDemo extends Object {
+    public static void main(String[] args) {
+        ObjectClassDemo demo = new ObjectClassDemo();
+        ObjectClassDemo demo2 = new ObjectClassDemo();
+        System.out.println(demo.getClass());
+        System.out.println(demo.toString());
+        System.out.println(demo);
+        System.out.println(Integer.toHexString(1265094477));
+        System.out.println(demo.hashCode());
+        System.out.println(demo.equals(demo2));
+        demo= demo2;
+        System.out.println(demo.equals(demo2));
+        System.out.println(demo.hashCode()+" "+demo2.hashCode());
+    }
+
+    @Override
+    public String toString() {
+        return "some value";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Finalize method");
+    }
+}
